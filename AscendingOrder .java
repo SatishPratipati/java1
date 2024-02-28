2e7f4ec82966313dfd262abb2a6a7feb9d1b6614
SatishPratipati <161418393+SatishPratipati@users.noreply.github.com>
2024-02-28 23:59:43 +0530

    Add files via upload

---
+++ AscendingOrder .java
@@ -0,0 +1,34 @@
+public class AscendingOrder {
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 10;
+        int num3 = 30;
+
+        System.out.println("Before sorting:");
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+        System.out.println("num3 = " + num3);
+
+        // Sorting logic
+        if (num1 > num2) {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+        if (num2 > num3) {
+            int temp = num2;
+            num2 = num3;
+            num3 = temp;
+        }
+        if (num1 > num2) {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+
+        System.out.println("\nAfter sorting in ascending order:");
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+        System.out.println("num3 = " + num3);
+    }
+}
